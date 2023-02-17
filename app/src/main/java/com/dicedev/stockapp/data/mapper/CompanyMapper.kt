e10725757f797e2b4b9f4f1d39c7bbc40b00d25a
Daniel Melo <daniel.melo42@outlook.com>
2023-02-17 16:54:04 -0700

    toCompanyListingEntity added

--- app/src/main/java/com/dicedev/stockapp/data/mapper/CompanyMapper.kt
+++ app/src/main/java/com/dicedev/stockapp/data/mapper/CompanyMapper.kt
@@ -6,3 +6,7 @@ import com.dicedev.stockapp.domain.model.CompanyListing
 fun CompanyListingEntity.toCompanyListing(): CompanyListing {
     return CompanyListing(name = name, symbol = symbol, exchange = exchange)
 }
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(name = name, symbol = symbol, exchange = exchange)
+}
