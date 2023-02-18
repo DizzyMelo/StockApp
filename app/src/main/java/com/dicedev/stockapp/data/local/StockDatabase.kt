e3802eaa7f676d84f1cf6148c077b1ee78e43107
Daniel Melo <daniel.melo42@outlook.com>
2023-02-18 10:40:56 -0700

    exportSchema property set to false

--- app/src/main/java/com/dicedev/stockapp/data/local/StockDatabase.kt
+++ app/src/main/java/com/dicedev/stockapp/data/local/StockDatabase.kt
@@ -3,7 +3,7 @@ package com.dicedev.stockapp.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [CompanyListingEntity::class], version = 1)
+@Database(entities = [CompanyListingEntity::class], version = 1, exportSchema = false)
 abstract class StockDatabase : RoomDatabase() {
     abstract fun stockDao(): StockDao
 }
