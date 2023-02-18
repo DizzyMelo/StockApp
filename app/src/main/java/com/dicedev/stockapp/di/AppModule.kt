f99bb95dcb3ae8bd935087ba94770d994db7df6e
Daniel Melo <daniel.melo42@outlook.com>
2023-02-18 09:54:46 -0700

    providing dependency injections

--- app/src/main/java/com/dicedev/stockapp/di/AppModule.kt
+++ app/src/main/java/com/dicedev/stockapp/di/AppModule.kt
@@ -1,10 +1,58 @@
 package com.dicedev.stockapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.dicedev.stockapp.data.csv.CompanyListingsParser
+import com.dicedev.stockapp.data.local.StockDao
+import com.dicedev.stockapp.data.local.StockDatabase
+import com.dicedev.stockapp.data.remote.StockApi
+import com.dicedev.stockapp.data.repository.StockRepositoryImpl
+import com.dicedev.stockapp.domain.repository.StockRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideStockDao(stockDatabase: StockDatabase): StockDao = stockDatabase.stockDao()
+
+    @Singleton
+    @Provides
+    fun provideStockDatabase(@ApplicationContext context: Context): StockDatabase {
+        return Room.databaseBuilder(context, StockDatabase::class.java, "stock_database")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideStockApi(): StockApi {
+        return Retrofit.Builder()
+            .baseUrl(StockApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(StockApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCSVParser(): CompanyListingsParser = CompanyListingsParser()
+
+    @Singleton
+    @Provides
+    fun provideStockRepository(
+        api: StockApi,
+        dao: StockDao,
+        parser: CompanyListingsParser
+    ): StockRepository =
+        StockRepositoryImpl(api, dao, parser)
 }
